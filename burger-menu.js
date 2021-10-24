59ffe96e4b414358b752d8eb7b8cf861c2a08403
skopchik <65411869+skopchik@users.noreply.github.com>
2021-10-24 23:55:31 +0300

    disable scroll

--- burger-menu.js
+++ burger-menu.js
@@ -2,10 +2,33 @@
 const menuBtn = document.querySelector(".menu-button");
 const menu = document.querySelector(".navbar-panel");
 const menuCloseBtn = document.querySelector(".close-menu");
+
+const disableScroll = () => {
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+    document.body.dbScrollY = window.scrollY;
+    document.body.style.cssText = `
+          position: fixed;
+          width: 100%;
+          height: 100vh;
+          left: 0;
+          top: ${-window.scrollY}px;
+          overflow: hidden;
+          padding-right: ${widthScroll}px;
+      `;
+};
+
+const enableScroll = () => {
+    document.body.style.cssText = "";
+    window.scroll({
+        top: document.body.dbScrollY,
+    });
+};
+
 menuBtn.addEventListener("click", () => {
     menu.classList.toggle("is-open");
+    disableScroll();
 });
 menuCloseBtn.addEventListener("click", () => {
     menu.classList.remove("is-open");
-
+    enableScroll();
 });
